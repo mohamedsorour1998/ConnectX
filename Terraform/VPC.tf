b53f31a6712e49e588cb6c43cab0fecb0e81a509
mohamedsorour1998 <mohamedsorour1998@gmail.com>
2023-08-20 22:55:31 +0300

    added 2 subnets in the VPC so EKS can launch

--- Terraform/VPC.tf
+++ Terraform/VPC.tf
@@ -1,32 +1,45 @@
 # this file will create a VPC with all of its components
 resource "aws_vpc" "main" {
   cidr_block = "10.0.0.0/16"
-    tags = {
-        Name = "Jenkins"
-    }
+  tags = {
+    Name = "ConnectX-VPC"
+  }
 }
+# this will create 2 subnets in the VPC with different az and cidr blocks in us-east-1
+
 resource "aws_subnet" "main" {
-  vpc_id     = aws_vpc.main.id
-  cidr_block = "10.0.0.0/24"
-    tags = {
-        Name = "main"
-    }
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.0.7.0/24"
+  availability_zone = "us-east-1a"
+  tags = {
+    Name = "main"
+  }
 }
+resource "aws_subnet" "main2" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.0.8.0/24"
+  availability_zone = "us-east-1b"
+  tags = {
+    Name = "main2"
+  }
+}
+# this will create an internet gateway and attach it to the VPC
+
 resource "aws_internet_gateway" "main" {
   vpc_id = aws_vpc.main.id
-    tags = {
-        Name = "main"
-    }
+  tags = {
+    Name = "main"
+  }
 }
 resource "aws_route_table" "main" {
   vpc_id = aws_vpc.main.id
-    route {
-        cidr_block = "0.0.0.0/0"
-        gateway_id = aws_internet_gateway.main.id
-    }
-    tags = {
-        Name = "main"
-    }
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.main.id
+  }
+  tags = {
+    Name = "main"
+  }
 }
 resource "aws_route_table_association" "main" {
   subnet_id      = aws_subnet.main.id
