4aa891d0f339649cd73944cf495317df74af0a63
mohamedsorour1998 <mohamedsorour1998@gmail.com>
2023-08-21 22:48:38 +0300

    Updates to terraform github actions

--- Terraform/VPC.tf
+++ Terraform/VPC.tf
@@ -45,3 +45,7 @@ resource "aws_route_table_association" "main" {
   subnet_id      = aws_subnet.main.id
   route_table_id = aws_route_table.main.id
 }
+resource "aws_route_table_association" "main2" {
+  subnet_id      = aws_subnet.main2.id
+  route_table_id = aws_route_table.main.id
+}
