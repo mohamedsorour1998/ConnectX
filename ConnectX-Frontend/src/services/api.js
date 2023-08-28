7f3328d99b24c1aa58544e18ecfcb523f150bad5
mohamedsorour1998 <mohamedsorour1998@gmail.com>
2023-08-28 14:35:32 +0300

    edited api part in react, adding http before the url

--- ConnectX-Frontend/src/services/api.js
+++ ConnectX-Frontend/src/services/api.js
@@ -3,7 +3,7 @@ import { loadToken } from "./auth";
 
 const api = axios.create({
   baseURL:
-    "aa2809da0a6cb45779dd895d24e4d8bc-230234518.us-east-1.elb.amazonaws.com",
+    "http://aa2809da0a6cb45779dd895d24e4d8bc-230234518.us-east-1.elb.amazonaws.com",
   headers: {
     "Content-Type": "application/json",
   },
